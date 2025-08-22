43904595afe1c6cf25dade36c851401bb2e4c15a
bhavin23012001 <makbhavin01@gmail.com>
2025-08-22 12:42:10 +0530

    Update location_manager_test.dart

--- test/location_manager_test.dart
+++ test/location_manager_test.dart
@@ -1,25 +1,33 @@
+import 'dart:async';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:geolocator_platform_interface/geolocator_platform_interface.dart';
 import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
 import 'package:CBT_APP/lib/managers/location_manager.dart';
+
 // Generate mocks with: flutter pub run build_runner build
-// Or manually create mock for GeolocatorPlatform
 class MockGeolocatorPlatform extends Mock implements GeolocatorPlatform {}
+
 @GenerateMocks([GeolocatorPlatform])
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   late MockGeolocatorPlatform mockGeolocator;
   late LocationManager locationManager;
+
   setUp(() {
     mockGeolocator = MockGeolocatorPlatform();
     GeolocatorPlatform.instance = mockGeolocator;
     locationManager = LocationManager();
   });
+
+  tearDown(() {
+    locationManager.stopListening();
+  });
+
   test('startListening returns early if location services disabled', () async {
     when(mockGeolocator.isLocationServiceEnabled())
         .thenAnswer((_) async => false);
-    // Should print and return without starting subscription
     bool called = false;
     await locationManager.startListening((position) {
       called = true;
@@ -27,6 +35,7 @@ void main() {
     expect(called, false);
     verify(mockGeolocator.isLocationServiceEnabled()).called(1);
   });
+
   test('startListening returns early if permission denied', () async {
     when(mockGeolocator.isLocationServiceEnabled())
         .thenAnswer((_) async => true);
@@ -45,19 +54,23 @@ void main() {
       mockGeolocator.requestPermission(),
     ]);
   });
+
   test('startListening subscribes to location stream if permitted', () async {
     when(mockGeolocator.isLocationServiceEnabled())
         .thenAnswer((_) async => true);
     when(mockGeolocator.checkPermission())
         .thenAnswer((_) async => LocationPermission.always);
+
     final controller = StreamController<Position>();
     when(mockGeolocator.getPositionStream(
       locationSettings: anyNamed('locationSettings'),
     )).thenAnswer((_) => controller.stream);
+
     Position? receivedPosition;
     await locationManager.startListening((position) {
       receivedPosition = position;
     });
+
     // Emit a fake position
     final position = Position(
       longitude: 10.0,
@@ -68,10 +81,14 @@ void main() {
       heading: 0,
       speed: 0,
       speedAccuracy: 0,
+      altitudeAccuracy: 0,
+      headingAccuracy: 0,
     );
     controller.add(position);
+
     await Future.delayed(Duration.zero);
     expect(receivedPosition, position);
+
     await locationManager.stopListening();
     await controller.close();
   });
