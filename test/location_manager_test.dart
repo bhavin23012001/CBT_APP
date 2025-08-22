88c37e87088f28632b1d9cf9d6c08bdf1bf453a0
bhavin23012001 <makbhavin01@gmail.com>
2025-08-22 13:07:08 +0530

    Update location_manager_test.dart

--- test/location_manager_test.dart
+++ test/location_manager_test.dart
@@ -2,76 +2,109 @@ import 'dart:async';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geolocator_platform_interface/geolocator_platform_interface.dart';
-import 'package:mockito/mockito.dart';
-import 'package:mockito/annotations.dart';
 import 'package:bus_route_finder/managers/location_manager.dart';
 
-// Generate mocks with: flutter pub run build_runner build
-class MockGeolocatorPlatform extends Mock implements GeolocatorPlatform {}
+// Custom mock extending GeolocatorPlatform instead of using implements
+class MockGeolocatorPlatform extends GeolocatorPlatform {
+  Function? _isLocationServiceEnabledHandler;
+  Function? _checkPermissionHandler;
+  Function? _requestPermissionHandler;
+  Function? _getPositionStreamHandler;
 
-@GenerateMocks([GeolocatorPlatform])
+  @override
+  Future<bool> isLocationServiceEnabled() {
+    return _isLocationServiceEnabledHandler?.call() ?? Future.value(false);
+  }
+
+  @override
+  Future<LocationPermission> checkPermission() {
+    return _checkPermissionHandler?.call() ?? Future.value(LocationPermission.denied);
+  }
+
+  @override
+  Future<LocationPermission> requestPermission() {
+    return _requestPermissionHandler?.call() ?? Future.value(LocationPermission.denied);
+  }
+
+  @override
+  Stream<Position> getPositionStream({required LocationSettings locationSettings}) {
+    return _getPositionStreamHandler?.call() ?? Stream<Position>.empty();
+  }
+
+  // Helper setters to inject mocked behavior
+  void setIsLocationServiceEnabledHandler(Future<bool> Function() handler) {
+    _isLocationServiceEnabledHandler = handler;
+  }
+
+  void setCheckPermissionHandler(Future<LocationPermission> Function() handler) {
+    _checkPermissionHandler = handler;
+  }
+
+  void setRequestPermissionHandler(Future<LocationPermission> Function() handler) {
+    _requestPermissionHandler = handler;
+  }
+
+  void setGetPositionStreamHandler(Stream<Position> Function() handler) {
+    _getPositionStreamHandler = handler;
+  }
+}
+
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+
   late MockGeolocatorPlatform mockGeolocator;
   late LocationManager locationManager;
 
   setUp(() {
     mockGeolocator = MockGeolocatorPlatform();
-    GeolocatorPlatform.instance = mockGeolocator; // Fixed typo here
+    GeolocatorPlatform.instance = mockGeolocator;
     locationManager = LocationManager();
   });
 
   tearDown(() {
-    locationManager.stopListening(); // No await because stopListening returns void
+    locationManager.stopListening();
   });
 
   test('startListening returns early if location services disabled', () async {
-    when(mockGeolocator.isLocationServiceEnabled())
-        .thenAnswer((_) async => false);
+    mockGeolocator.setIsLocationServiceEnabledHandler(() => Future.value(false));
+
     bool called = false;
-    locationManager.startListening((position) { // Removed await, method returns void
+    locationManager.startListening((position) {
       called = true;
     });
+
+    await Future.delayed(Duration.zero); // Allow event loop
+
     expect(called, false);
-    verify(mockGeolocator.isLocationServiceEnabled()).called(1);
   });
 
   test('startListening returns early if permission denied', () async {
-    when(mockGeolocator.isLocationServiceEnabled())
-        .thenAnswer((_) async => true);
-    when(mockGeolocator.checkPermission())
-        .thenAnswer((_) async => LocationPermission.denied);
-    when(mockGeolocator.requestPermission())
-        .thenAnswer((_) async => LocationPermission.denied);
+    mockGeolocator.setIsLocationServiceEnabledHandler(() => Future.value(true));
+    mockGeolocator.setCheckPermissionHandler(() => Future.value(LocationPermission.denied));
+    mockGeolocator.setRequestPermissionHandler(() => Future.value(LocationPermission.denied));
+
     bool called = false;
-    locationManager.startListening((position) { // Removed await
+    locationManager.startListening((position) {
       called = true;
     });
+
+    await Future.delayed(Duration.zero);
+
     expect(called, false);
-    verifyInOrder([
-      mockGeolocator.isLocationServiceEnabled(),
-      mockGeolocator.checkPermission(),
-      mockGeolocator.requestPermission(),
-    ]);
   });
 
   test('startListening subscribes to location stream if permitted', () async {
-    when(mockGeolocator.isLocationServiceEnabled())
-        .thenAnswer((_) async => true);
-    when(mockGeolocator.checkPermission())
-        .thenAnswer((_) async => LocationPermission.always);
+    mockGeolocator.setIsLocationServiceEnabledHandler(() => Future.value(true));
+    mockGeolocator.setCheckPermissionHandler(() => Future.value(LocationPermission.always));
 
     final controller = StreamController<Position>();
-    when(mockGeolocator.getPositionStream(  // Fixed typo here
-      locationSettings: anyNamed('locationSettings'),
-    )).thenAnswer((_) => controller.stream);
+    mockGeolocator.setGetPositionStreamHandler(() => controller.stream);
 
     Position? receivedPosition;
-    locationManager.startListening((position) { // Removed await
+    locationManager.startListening((position) {
       receivedPosition = position;
     });
 
-    // Emit a fake position
     final position = Position(
       longitude: 10.0,
       latitude: 20.0,
@@ -87,10 +120,10 @@ void main() {
 
     controller.add(position);
     await Future.delayed(Duration.zero);
+
     expect(receivedPosition, position);
 
-    locationManager.stopListening(); // Removed await
-
+    locationManager.stopListening();
     await controller.close();
   });
 }
